bd59fe2814041afb50f2bdf01b1f684554792ee8
sean-gribben <sean.gribben5@gmail.com>
2020-12-25 00:49:03 +1100

    Added basic title screen scene (doesn't work yet)

---
+++ Assets/Scripts/Highlight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highlight : MonoBehaviour
+{
+	TextMesh text;
+	void Start()
+	{
+		text = GetComponent<TextMesh>();
+	}
+
+    void OnMouseEnter()
+    {
+        text.color = Color.red;
+    }
+
+    void OnMouseExit()
+    {
+        text.color = Color.white;
+    }
+}
